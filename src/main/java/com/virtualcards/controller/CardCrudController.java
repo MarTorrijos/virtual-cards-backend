99931155b03ad48e904010f9f393265f183ce97b
mar <martorrijos@protonmail.com>
2025-07-07 12:05:17 +0200

    feat: update CardCrud service and controller

---
+++ src/main/java/com/virtualcards/controller/CardCrudController.java
@@ -0,0 +1,45 @@
+package com.virtualcards.controller;
+
+import com.virtualcards.dto.CreateCardRequest;
+import com.virtualcards.model.Card;
+import com.virtualcards.service.CardCrudService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/card")
+public class CardCrudController {
+
+    private final CardCrudService cardCrudService;
+
+    public CardCrudController(CardCrudService cardCrudService) {
+        this.cardCrudService = cardCrudService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Card> createCard(@RequestBody CreateCardRequest request) {
+        Card addedCard = cardCrudService.createCard(request.getType(), request.getUserId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedCard);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCard(@PathVariable Long id) {
+        cardCrudService.deleteCard(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Card> getCard(@PathVariable Long id) {
+        Card card = cardCrudService.getCard(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(card);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Card>> getAllCards() {
+        List<Card> cards = cardCrudService.getAllCards();
+        return ResponseEntity.ok(cards);
+    }
+
+}
--- src/main/java/com/virtualcards/service/CardCrudService.java
+++ src/main/java/com/virtualcards/service/CardCrudService.java
@@ -2,6 +2,8 @@ package com.virtualcards.service;
 
 import com.virtualcards.exception.CardNotFoundException;
 import com.virtualcards.model.Card;
+import com.virtualcards.model.enums.Type;
+import com.virtualcards.model.factory.CardFactory;
 import com.virtualcards.repository.CardRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +13,22 @@ import java.util.List;
 public class CardCrudService {
 
     private final CardRepository cardRepository;
+    private final CardFactory cardFactory;
 
-    public CardCrudService(CardRepository cardRepository) {
+    public CardCrudService(CardRepository cardRepository, CardFactory cardFactory) {
         this.cardRepository = cardRepository;
+        this.cardFactory = cardFactory;
     }
 
     // TODO: possibly more validations
 
-    public Card createCard(Card card) {
+    public Card createCard(Type type, Long userId) {
+        Card card = cardFactory.createCard(type, userId);
         return cardRepository.save(card);
     }
 
     public void deleteCard(Long id) {
-        // TODO: check if cards exists, then delete it
+        Card card = getCard(id);
         cardRepository.deleteById(id);
     }
 
