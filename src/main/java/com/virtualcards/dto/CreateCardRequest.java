3e462f7c13cdf7db9ed0c8dcd56f4eaf587e2084
mar <martorrijos@protonmail.com>
2025-07-07 12:05:34 +0200

    feat: CreateCardRequest dto

---
+++ src/main/java/com/virtualcards/dto/CreateCardRequest.java
@@ -0,0 +1,14 @@
+package com.virtualcards.dto;
+
+import com.virtualcards.model.enums.Type;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateCardRequest {
+
+    private Type type;
+    private Long userId;
+
+}
