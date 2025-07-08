a583decd83c63c5a8c83149a8484a68403fbe4e7
mar <martorrijos@protonmail.com>
2025-07-08 12:54:31 +0200

    chore: small changes

--- src/test/java/com/virtualcards/Sprint52VirtualCardsApplicationTests.java
+++ src/test/java/com/virtualcards/Sprint52VirtualCardsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Sprint52VirtualCardsApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
