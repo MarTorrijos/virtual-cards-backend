8911fc47b5336d1cf222d687390072fcf3200f76
mar <martorrijos@protonmail.com>
2025-07-09 11:32:30 +0200

    update CardCombatService

--- src/main/java/com/virtualcards/service/battle/CardCombatService.java
+++ src/main/java/com/virtualcards/service/battle/CardCombatService.java
@@ -1,35 +1,58 @@
 package com.virtualcards.service.battle;
 
 import com.virtualcards.domain.Card;
-import com.virtualcards.repository.CardRepository;
+import com.virtualcards.domain.enums.Type;
+import com.virtualcards.dto.card.OpponentCard;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class CardCombatService {
 
-    private final CardRepository cardRepository;
+    private final Random random = new Random();
 
-    public CardCombatService(CardRepository cardRepository) {
-        this.cardRepository = cardRepository;
+    public boolean doesPlayerBeginAttacking() {
+        return random.nextBoolean();
     }
 
-    public Card attack() {
-        // TODO: finish this. null is just a placeholder
-        return null;
+    public int cardAttack(Card card, OpponentCard opponentCard) {
+        int effectiveAttack = calculateEffectiveAttack(card.getType(), opponentCard.getType(),
+                card.getAttack());
+        return applyDamage(opponentCard.getHealth(), effectiveAttack);
     }
 
-    public Card loseHealth(Card card) {
-        // TODO: check if card exists
-        return cardRepository.save(card);
+    public int opponentCardAttack(Card card, OpponentCard opponentCard) {
+        int effectiveAttack = calculateEffectiveAttack(opponentCard.getType(), card.getType(),
+                opponentCard.getAttack());
+        return applyDamage(card.getHealth(), effectiveAttack);
     }
 
-    public Card winXP() {
-        // TODO: finish this. null is just a placeholder
-        return null;
+    private int applyDamage(int defenderHealth, int attackerEffectiveAttack) {
+        return defenderHealth - attackerEffectiveAttack;
     }
 
-    public void determineWinner() {
-        // TODO: finish this
+    private int calculateEffectiveAttack(Type attackerType, Type defenderType, int baseAttack) {
+        return hasTypeAdvantage(attackerType, defenderType) ? baseAttack + 10 : baseAttack;
+    }
+
+    private boolean hasTypeAdvantage(Type attacker, Type defender) {
+        return (attacker == Type.ROCK && defender == Type.SCISSORS)
+                || (attacker == Type.SCISSORS && defender == Type.PAPER)
+                || (attacker == Type.PAPER && defender == Type.ROCK);
+    }
+
+    public int winXP(Card card) {
+        return switch (card.getEvolutionStage()) {
+            case 1 -> 30;
+            case 2 -> 25;
+            case 3 -> 20;
+            default -> 0;
+        };
+    }
+
+    public boolean doesPlayerCardWin(Card card, OpponentCard opponentCard) {
+        return opponentCard.getHealth() <= 0;
     }
 
 }
