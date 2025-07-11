9fa944efbf5dc31076f2ce2293197ca027e613df
mar <martorrijos@protonmail.com>
2025-07-11 13:43:51 +0200

    feat: create new user DTOs

---
+++ src/main/java/com/virtualcards/dto/user/UpdatePasswordRequestDto.java
@@ -0,0 +1,14 @@
+package com.virtualcards.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordRequestDto {
+
+    private String newPassword;
+
+}
---
+++ src/main/java/com/virtualcards/dto/user/UpdateUsernameRequestDto.java
@@ -0,0 +1,14 @@
+package com.virtualcards.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUsernameRequestDto {
+
+    private String newUsername;
+
+}
