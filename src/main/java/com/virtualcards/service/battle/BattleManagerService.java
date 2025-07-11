04711e232a43aa1df7df6b7a898bf65e2155d901
mar <martorrijos@protonmail.com>
2025-07-11 09:35:22 +0200

    finish BattleManagerService

--- src/main/java/com/virtualcards/service/battle/BattleManagerService.java
+++ src/main/java/com/virtualcards/service/battle/BattleManagerService.java
@@ -1,7 +1,9 @@
 package com.virtualcards.service.battle;
 
 import com.virtualcards.domain.Card;
+import com.virtualcards.dto.battle.BattleLog;
 import com.virtualcards.dto.card.OpponentCard;
+import com.virtualcards.util.BattleLogger;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -9,68 +11,85 @@ import java.util.Random;
 @Service
 public class BattleManagerService {
 
-    private final CardCombatService cardCombatService;
+    private final AttackService cardCombatService;
     private final OpponentCardGenerator opponentCardGenerator;
+    private final CombatResultService combatResultService;
 
-    public BattleManagerService(CardCombatService cardCombatService, OpponentCardGenerator opponentCardGenerator) {
+
+    public BattleManagerService(AttackService cardCombatService, OpponentCardGenerator opponentCardGenerator,
+                                CombatResultService combatResultService) {
         this.cardCombatService = cardCombatService;
         this.opponentCardGenerator = opponentCardGenerator;
+        this.combatResultService = combatResultService;
     }
 
-    public Card battle(Card card) {
-        OpponentCard opponentCard = generateFairOpponent(card);
-        fightLoop(card, opponentCard);
-        resolveOutcome();
+    public BattleLog battle(Card card) {
+        BattleLogger logger = new BattleLogger();
 
-        return card;
+        OpponentCard opponentCard = generateFairOpponent(card, logger);
+        fightLoop(card, opponentCard, logger);
+        resolveOutcome(card, opponentCard, logger);
+
+        return new BattleLog(card, logger.getEvents());
     }
 
-    private OpponentCard generateFairOpponent(Card card) {
+    private OpponentCard generateFairOpponent(Card card, BattleLogger logger) {
         OpponentCard opponentCard = opponentCardGenerator.createRandomOpponent();
 
         switch (card.getEvolutionStage()) {
             case 1 -> opponentCard.setEvolutionStage(randomBetween(1, 2));
             case 2 -> opponentCard.setEvolutionStage(randomBetween(2, 3));
             case 3 -> opponentCard.setEvolutionStage(3);
-            default -> System.out.println("Incorrect card evolution stage");
         }
 
+        logger.logOpponentGenerated(opponentCard.getName(), opponentCard.getEvolutionStage());
         return opponentCard;
     }
 
-    private void fightLoop(Card card, OpponentCard opponentCard) {
-        if (cardCombatService.doesPlayerBeginAttacking()) {
-            while (true) {
-                int newOpponentHealth = cardCombatService.cardAttack(card, opponentCard);
-                opponentCard.setHealth(newOpponentHealth);
-                if (newOpponentHealth <= 0) break;
+    private void fightLoop(Card card, OpponentCard opponentCard, BattleLogger logger) {
+        boolean playerStarts = cardCombatService.doesPlayerBeginAttacking();
+        String starterName = playerStarts ? card.getName() : opponentCard.getName();
+        logger.logStart(starterName);
 
-                int newPlayerHealth = cardCombatService.opponentCardAttack(card, opponentCard);
-                card.setHealth(newPlayerHealth);
-                if (newPlayerHealth <= 0) break;
-            }
-        } else {
-            while (true) {
-                int newPlayerHealth = cardCombatService.opponentCardAttack(card, opponentCard);
-                card.setHealth(newPlayerHealth);
-                if (newPlayerHealth <= 0) break;
+        boolean isBattleOver = false;
 
-                int newOpponentHealth = cardCombatService.cardAttack(card, opponentCard);
-                opponentCard.setHealth(newOpponentHealth);
-                if (newOpponentHealth <= 0) break;
+        while (!isBattleOver) {
+            isBattleOver = processBattleTurn(card, opponentCard, playerStarts, logger);
+            if (!isBattleOver) {
+                isBattleOver = processBattleTurn(card, opponentCard, !playerStarts, logger);
             }
         }
     }
 
-    private void resolveOutcome() {
-        // TODO
-            // if player wins
-                // award xp
-                // it will have a waiting period until the health is fully restored
-                // (or not, but fighting with less health is not recommended)
-            // if player loses
-                // no xp
-                // health drops to 0
+    private boolean processBattleTurn(Card card, OpponentCard opponentCard, boolean isPlayerTurn, BattleLogger logger) {
+        return executeTurn(card, opponentCard, isPlayerTurn, logger);
+    }
+
+    private boolean executeTurn(Card card, OpponentCard opponentCard, boolean isPlayer, BattleLogger logger) {
+        if (isPlayer) {
+            int newOpponentHealth = cardCombatService.cardAttack(card, opponentCard);
+            newOpponentHealth = Math.max(newOpponentHealth, 0);
+            opponentCard.setHealth(newOpponentHealth);
+            logger.logAttack(card.getName(), opponentCard.getName(), newOpponentHealth);
+            return newOpponentHealth == 0;
+        } else {
+            int newPlayerHealth = cardCombatService.opponentCardAttack(card, opponentCard);
+            newPlayerHealth = Math.max(newPlayerHealth, 0);
+            card.setHealth(newPlayerHealth);
+            logger.logAttack(opponentCard.getName(), card.getName(), newPlayerHealth);
+            return newPlayerHealth == 0;
+        }
+    }
+
+    private void resolveOutcome(Card card, OpponentCard opponentCard, BattleLogger logger) {
+        if (combatResultService.playerCardWins(opponentCard)) {
+            int xpGained = combatResultService.winXP(card);
+            card.setXp(card.getXp() + xpGained);
+            logger.logWin(card.getName());
+            logger.logXpGain(card.getName(), xpGained);
+        } else {
+            logger.logLoss(card.getName());
+        }
     }
 
     private int randomBetween(int min, int max) {
