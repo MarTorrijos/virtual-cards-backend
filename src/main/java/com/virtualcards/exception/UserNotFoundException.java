4a470378b52dbf3cfc3cbe16226fb1065c9cd66f
mar <martorrijos@protonmail.com>
2025-07-12 09:08:13 +0200

    feat: UserNotFoundException

---
+++ src/main/java/com/virtualcards/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.virtualcards.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
