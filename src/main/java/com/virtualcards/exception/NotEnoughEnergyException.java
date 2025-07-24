255543a0dcca9a27b36e2f4c9c8cb37cad051880
mar <martorrijos@protonmail.com>
2025-07-24 13:37:37 +0200

    chore: delete files

--- src/main/java/com/virtualcards/exception/NotEnoughEnergyException.java
+++
@@ -1,9 +0,0 @@
-package com.virtualcards.exception;
-
-public class NotEnoughEnergyException extends RuntimeException {
-
-    public NotEnoughEnergyException(String message) {
-        super(message);
-    }
-
-}
